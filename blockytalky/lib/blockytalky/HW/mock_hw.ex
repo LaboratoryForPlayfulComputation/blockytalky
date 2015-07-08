ea961ac6fa7bd69673ba17635cc7c3cbbb2c8cf6
Matthew Ahrens <Matt.P.Ahrens@gmail.com>
2015-07-08 19:52:45 -0400

    typo

--- blockytalky/lib/blockytalky/HW/mock_hw.ex
+++ blockytalky/lib/blockytalky/HW/mock_hw.ex
@@ -10,11 +10,12 @@ defmodule Blockytalky.MockHW do
   ####
   # External API
 
-  def get_sensor_value(port_id), do
+  def get_sensor_value(port_id) do
     case PythonQuerier.run_result(:mock, :get_sensor_value,[get_port_num(port_id)]) do
       {_, value} -> value
       v -> v
     end
+  end
   def set_sensor_type(port_id, value), do: PythonQuerier.run(:mock, :set_sensor_type, [get_port_num(port_id), value])
   def get_port_num(port_id), do: Map.get(port_map, port_id)
 end
