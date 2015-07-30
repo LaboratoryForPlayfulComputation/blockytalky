028e23f728d216b3d66a5cf3244ec588774a67e7
Matt Ahrens <matthew.ahrens@tufts.edu>
2015-07-30 01:26:00 -0400

    stage for 0.2.1

--- blockytalky/mix.exs
+++ blockytalky/mix.exs
@@ -3,7 +3,7 @@ defmodule Blockytalky.Mixfile do
 
   def project do
     [app: :blockytalky,
-     version: "0.2.0",
+     version: "0.2.1",
      elixir: "~> 1.0",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix] ++ Mix.compilers,
