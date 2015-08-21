2edbbea9b527688cec4cc492503fb29e9aa01367
Matt Ahrens <matthew.ahrens@tufts.edu>
2015-08-20 22:01:42 -0400

    Upgraded phoenix to 0.17 and fixed error with message channel

--- blockytalky/web/static/js/app.js
+++ blockytalky/web/static/js/app.js
@@ -1,4 +1,5 @@
-import {Socket} from "phoenix"
+import {Socket} from "deps/phoenix/web/static/js/phoenix"
+import "deps/phoenix_html/web/static/js/phoenix_html"
 /* after page loads DOM elements */
  //common page elements:
  //humane is the popup notification for logging messages to the client
