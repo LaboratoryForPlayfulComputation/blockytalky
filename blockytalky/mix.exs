f88767790541de2d21857fd4be2e76313528b2e7
Matt Ahrens <matthew.ahrens@tufts.edu>
2015-12-02 21:56:55 -0500

    exrm progress

--- blockytalky/mix.exs
+++ blockytalky/mix.exs
@@ -44,8 +44,8 @@ defmodule Blockytalky.Mixfile do
      { :exjsx, git: "https://github.com/talentdeficit/exjsx.git" },
      { :socket, git: "https://github.com/meh/elixir-socket.git"},
      {:exrm, "~> 1.0.0-rc7", override: true},
-     {:conform, "~> 1.0.0-rc8", override: true},
-     {:conform_exrm, "~> 0.1"}
+     {:conform, git: "https://github.com/bitwalker/conform", tag: "1.0.0-rc8", override: true},
+     {:conform_exrm, "~> 0.2"}
    ]
   end
 end
