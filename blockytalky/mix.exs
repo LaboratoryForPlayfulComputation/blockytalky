81cdd8ec96912a1a931dda8e9c04132d550b691b
shadowfax6894 <anniekelly6894@gmail.com>
2016-02-25 11:42:32 -0700

    updating new rel

--- blockytalky/mix.exs
+++ blockytalky/mix.exs
@@ -3,7 +3,7 @@ defmodule Blockytalky.Mixfile do
 
   def project do
     [app: :blockytalky,
-     version: "0.4.0",
+     version: "0.4.3",
      elixir: "~> 1.0",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix] ++ Mix.compilers,
